1cc615cd22cdaf78da48342b196d93568a50fd83
bbigbros <jr.im@stripes.co.kr>
2016-06-14 22:50:22 +0900

    first commit

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Blog::Application.initialize!
+
+
+ActiveSupport::Deprecation.silenced = true
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '3afaed2b43988eb780b20b5271c42762ff6de5c07b374dae06a19a18fefad4ffb2767ab2349d713589ea802318af63577e6c3a5d7b38b3e7908c54c394a8cc8e'
